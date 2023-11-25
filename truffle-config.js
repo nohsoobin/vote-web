6c70cab37585d4872ffa86c7b31cac1baa9f34cf
nohsoobin <nohsoobin18@naver.com>
2023-11-25 23:45:33 +0900

    1125

--- truffle-config.js
+++ truffle-config.js
@@ -3,12 +3,18 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: '127.0.0.1',
+      host: "127.0.0.1",
       port: 7545,
       network_id: 5777, // Match any network id
     },
     develop: {
-      port: 8545,
+      port: 7545,
     },
   },
-}
+  compilers: {
+    solc: {
+      version: "0.8.19", // 사용할 Solidity 버전으로 업데이트
+      // 또는 "0.5.16" 등 원하는 버전으로 지정
+    },
+  },
+};
